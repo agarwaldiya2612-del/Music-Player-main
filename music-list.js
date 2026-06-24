99493ff86e04d0b06d2d9afa9b46016d6369f870
agarwaldiya2612-del <agarwaldiya2612@gmail.com>
2026-06-24 12:04:40 +0530

    Add files via upload

---
+++ music-list.js
@@ -0,0 +1,23 @@
+let allMusic = [
+  {
+    name: "Kesariya",
+    artist: "Arijit Singh",
+    src: "music-1"
+  },
+  {
+    name: "Heeriye",
+    artist: "Arijit Singh & Jasleen Royal",
+    src: "music-2"
+  },
+  
+  {
+    name: "Tum Hi Ho",
+    artist: "Arijit Singh",
+    src: "music-3"
+  },
+  {
+    name: "Raatan Lambiyan",
+    artist: "Jubin Nautiyal & Asees Kaur",
+    src: "music-4"
+  }
+];
